aa377d1ed5ab99bea0dd0de90a4bf09114f2568c
shshalam68 <a.k.mshahalam68@gmail.com>
2024-10-01 23:40:04 +0600

    first commit

---
+++ code.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main(){
+    int side ;
+    cout<< "enter side of a square :";
+    cin>> side ;
+    cout<< "area of square is:"<< side * side;
+    return 0; 
+} 
